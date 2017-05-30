3e4c62d540d72777d8ea3819aeb0c7709f4e22ec
Vatsala30 <vatsala3012@gmail.com>
2017-05-30 10:59:49 +0530

    Update joke.py

--- joke.py
+++ joke.py
@@ -1,50 +1,7 @@
 from random import shuffle
 import sys
 from wit import Wit
-################################################################################
-import os,sys
-from flask import Flask,request
-from pymessenger import Bot
 
-app = Flask(__name__)
-PAGE_ACCESS_TOKEN ="EAAEYD3T8JdIBAMj5uzoq2dANzB3dQZBuZBQ3ZAuTwZCX5AOPrxtJOdPsXHbx907ZAixUKfUvwH8KrtWdizZBIAxQ1Dy1DYeagIhd47yJNR9b52V8KfZAZAFtRRN6AiRYKix2h0nWrxZAE9O58N0gAhntyMmNoZArscAEY0Xt65Ij9opwZDZD"
-bot = Bot(PAGE_ACCESS_TOKEN)
-@app.route('/', methods=['GET'])
-def verify():
-      #Webhook Verification
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == "hello":
-            return "Verification token mismatch",403
-        return request.args["hub.challenge"],200
-    return "Hello World",200
-
-@app.route('/',methods=['POST'])
-def webhook():
-    data = request.get_json()
-    log(data)
-    if(data['object']) == 'page':
-       for entry in data['entry']:
-         for messaging_event in entry['messaging']:
-            sender_id = messaging_event['sender']['id']
-            recipient_id = messaging_event['recipient']['id']
-       
-            if messaging_event.get('message'):
-                if 'text' in messaging_event['message']:
-                    messaging_text=messaging_event['message']['text']
-                else:
-                    messaging_text='no text'
-            response = messaging_text
-            bot.send_text_message(sender_id,response)
-    return "ok",200
-
-    
-def log(message):
-    print(message)
-    sys.stdout.flush()
-
-if __name__ == "__main__":
-    app.run(debug = True, port = 80)
-###############################################################################
 access_token= "MGMGAD7W5MP2CQNPMZWJWY7RVE4VPR3R"
 
 all_jokes = {
