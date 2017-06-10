8660a3353615e32cd157fbdea673a6e4cb96f5be
shreya0196 <shreyag010196@gmail.com>
2017-06-10 23:06:50 +0530

    Revised Code

--- general_knowledge.py
+++ general_knowledge.py
@@ -5,14 +5,14 @@ def main(messaging_text):
     client = wolframalpha.Client("V63VYX-EX865W38Y4")
     res = client.query(messaging_text)
 
-    #count=0
-    #for pod in res.pods:
-        #if(count>=1):
-            #break
     try:
         return next(res.results).text
     except StopIteration:
-        print ("No results")
+            print ("No results")
+    except AttributeError:
+            return "I have no answer"
+		
+
 
 
 
